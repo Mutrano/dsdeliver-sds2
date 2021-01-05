a652ea24fe9036d5d27e8271d38de0b25794b48b
Mario Rabelo <mutrano@gmail.com>
2021-01-05 17:08:46 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsveliver/DsveliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsveliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsveliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsveliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsveliver/DsveliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsveliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsveliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
